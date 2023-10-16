3ce8bf013934b8be643f922e71ed6801ccb5c629
Christian Westgaard <comlock@gmail.com>
2023-10-16 10:54:41 +0200

    Fixed animateScrollTop

--- src/animateScrollTop.ts
+++ src/animateScrollTop.ts
@@ -1,24 +1,26 @@
 export function animateScrollTop(
-    el: HTMLElement,
-    targetTop: number,
-    durationMs: number,
-    complete: () => void
+	el: HTMLElement,
+	targetTop: number,
+	durationMs: number,
+	complete: () => void = () => {}
 ) {
-    const startScrollTop = el.scrollTop;
-    const distance = targetTop - startScrollTop;
-    const framesPerSecond = 50;
-    const timeout = 1000 / framesPerSecond;
-    const intervals = durationMs / timeout;
-    const step = distance / intervals;
-    const timer = setInterval(() => {
-        if (el.scrollTop >= targetTop) {
-            clearInterval(timer);
-            if (el.scrollTop > targetTop) { // Handle overshoot (decimals)
-                el.scrollTop = targetTop;
-            }
-            complete();
-        } else {
-            el.scrollTop += step;
-        }
-    }, timeout);
+	const startScrollTop = el.scrollTop;
+	const distance = targetTop - startScrollTop;
+	const framesPerSecond = 50;
+	const timeout = 1000 / framesPerSecond;
+	const intervals = durationMs / timeout;
+	const step = distance / intervals;
+	let start = Date.now();
+	const timer = setInterval(() => {
+		const timePassed = Date.now() - start;
+		if (timePassed >= durationMs) {
+			clearInterval(timer);
+			if (el.scrollTop !== targetTop) {
+				el.scrollTop = targetTop;
+			}
+			complete();
+		} else {
+			el.scrollTop += step;
+		}
+	}, timeout);
 }
